4df1c4f9bce96129cfdc5b62de8eaa3dd9ae2f3a
Usborn Ocampo <usborn116@gmail.com>
2024-03-04 12:01:50 -0800

    Form shows up when clicking edit and closes when clicking save

--- src/DateEntry.tsx
+++ src/DateEntry.tsx
@@ -13,14 +13,30 @@ export const DateEntry = ({ f, i, resetDates }: Props) => {
 
     const [edit, setEdit] = useState<boolean>(false)
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+
+        setEdit(false)
+    }
+
     if (edit){
         return (
+
+            
+            <form onSubmit={handleSubmit} id='editHourForm'>
+                <input type="date" name="date" id="vdate" defaultValue={f.date.toISOString().slice(0, 10)}/>
+                <input type='number' defaultValue={f.hrs}/>
+                <input type="submit" className='delete' value="Save"></input>
+            </form>
+            /*
             <div className={`row ${f.overMax ? 'over' : ''} ${f.payday ? '' : 'vacay'}`} key={i}>
                 <div>{i == 0 ? "Today" : f.date.toLocaleDateString('en-us', { timeZone: 'UTC'})}</div>
                 <div>{f.hrs}</div>
                 <div className={f.overMax ? 'over' : ''}>{f.totalHrs}</div>
                 { f.payday ? '' : <button className='delete' onClick={() => resetDates(f.date)}>X</button>}
             </div>
+            */
         )
     }
 
